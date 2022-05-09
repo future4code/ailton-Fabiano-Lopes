a0121066096de3c5590e4727b1a57e066ac7751f
fabianoclopes <103345612+fabianoclopes@users.noreply.github.com>
2022-05-09 13:57:32 -0300

    Exercicio de variavéis

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,40 +1,40 @@
-//let a = 10
-//let b = 10
+let a = 10
+let b = 10
 
-//console.log(b)
+console.log(b)
 
-//b = 5
-//console.log(a, b)
+b = 5
+console.log(a, b)
 
 /* primeiro console b será impresso com valor de 10, já no segundo como "let" é uma variável que pode ser mudada o valor de b passa a ser 5 e o valor de a continua 10*/
 
-/*let a = 10
+let a = 10
 let b = 20
 c = a
 b = c
 a = b
 
-console.log(a, b, c)*/
+console.log(a, b, c)
 
 /* 10 10 10, ao informar que todas variáveis são iguais logo b que era 20 também terá o mesmo valor das outras*/
 
 
-/*let p = prompt("Quantas horas você trabalha por dia?")
+let p = prompt("Quantas horas você trabalha por dia?")
 let t = prompt("Quanto você recebe por dia?")
-alert(`Voce recebe ${t/p} por hora`)*/
+alert(`Voce recebe ${t/p} por hora`)
 
 /* no p poderia colocar horasDia, e no t poderia pagDia, ele faz uma divisão entre a horas trabalhadas e valor do pagamento no dia e mostra o resultado de quanto ganha por hora*/
 
-/*let nome;
+let nome;
 let num;
-console.log (typeof nome,typeof num)*/
+console.log (typeof nome,typeof num)
 /* undefinied por que não foi declarado nenhum tipo de variável sendo impossivel detectar qual seria*/
 
 
-/*let nome = prompt("Qual é o seu nome")
+let nome = prompt("Qual é o seu nome")
 let idade = +prompt ("Qual é a sua idade")
 
-console.log (typeof nome, typeof idade)*/
+console.log (typeof nome, typeof idade)
 
 
 let per1= ("Está chovendo hoje?");
@@ -49,7 +49,7 @@ let res3= false;
 console.log (`${per1} ${res1 || "Não"}, ${per2} ${ "Sim"|| res2}, ${per3} ${res3 || "Não"}`)
 console.log (typeof res1, typeof res2, typeof res3)
 
-/*let a = 10;
+let a = 10;
 let b = 25;
 let c;
 
@@ -58,4 +58,4 @@ a = b;
 b=c;
 console.log (a)
 
-console.log (b)*/
+console.log (b)
